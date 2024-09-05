71e34fc93dddc896d46695c412b07881b8c105b0
Wendel <138839125+Wen-nunes@users.noreply.github.com>
2024-09-05 20:03:14 -0300

    Add files via upload

---
+++ Alunos/Ordenacao.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alunos
+{
+    internal class Ordenacao
+    {
+        public static void Bolha(List<Aluno> lista)
+        {
+            bool houveTroca = true;
+            while (houveTroca)
+            {
+                houveTroca = false;
+                for (int i = 0; i < lista.Count - 1; i++)
+                {
+                    if (lista[i].CompareTo(lista[i + 1]) > 0)   
+                    {
+                        houveTroca = true;
+                        Aluno tmp = lista[i];
+                        lista[i] = lista[i + 1];
+                        lista[i + 1] = tmp;
+                    }
+                }
+            }
+        }
+
+        public static void Selecao(List<Aluno> lista)
+        {
+            for (int i = 0; i < lista.Count - 1; i++)
+            {
+                int posMenor = i;
+                for (int j = i + 1; j < lista.Count; j++)
+                {
+                    if (lista[j].CompareTo(lista[posMenor]) < 0)
+                    {
+                        posMenor = j;
+                    }
+                }
+
+                if (i != posMenor)
+                {
+                    Aluno tmp = lista[i];
+                    lista[i] = lista[posMenor];
+                    lista[posMenor] = tmp;
+                }
+            }
+        }
+
+        public static void Insercao(List<Aluno> lista)
+        {
+            for (int i = 1; i < lista.Count; i++)
+            {
+                Aluno tmp = lista[i];
+                int j = i - 1;
+                while (j >= 0)
+                {
+                    if (tmp.CompareTo(lista[j]) < 0)
+                    {
+                        lista[j + 1] = lista[j];
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    j--;
+                }
+
+                lista[j + 1] = tmp;
+            }
+        }
+    }
+}
---
+++ Alunos/Program.cs
@@ -0,0 +1,54 @@
+﻿using Alunos;
+using System.Collections.Generic;
+using System.Threading;
+
+int tamanho = 60000;
+
+List<Aluno> listaBolha = Aluno.GerarAlunos(tamanho);
+List<Aluno> listaInsercao = Aluno.CopiarListaAlunos(listaBolha);
+List<Aluno> listaSelecao = Aluno.CopiarListaAlunos(listaBolha);
+List<Aluno> listaSort = Aluno.CopiarListaAlunos(listaBolha);
+
+void rodaBolha()
+{
+    Console.WriteLine("Ordenando a lista para o bolha...");
+    Ordenacao.Bolha(listaBolha);
+    Console.WriteLine("feito... com bolha");
+}
+
+void rodaSelecao()
+{
+    Console.WriteLine("Ordenando a lista para seleção...");
+    Ordenacao.Selecao(listaSelecao);
+    Console.WriteLine("feito... com seleção");
+}
+
+void rodaSort()
+{
+    Console.WriteLine("Ordenando a lista para o sort...");
+    listaSort.Sort((a, b) => a.Nome.CompareTo(b.Nome) != 0 ? a.Nome.CompareTo(b.Nome) : a.Idade.CompareTo(b.Idade));
+    Console.WriteLine("feito... sort C#");
+}
+
+void rodaInsercao()
+{
+    Console.WriteLine("Ordenando a lista para inserção...");
+    Ordenacao.Insercao(listaInsercao);
+    Console.WriteLine("feito... com inserção");
+}
+
+new Thread(
+    () => rodaBolha()
+).Start();
+
+new Thread(
+    () => rodaSelecao()
+).Start();
+
+new Thread(
+    () => rodaInsercao()
+).Start();
+
+new Thread(
+    () => rodaSort()
+).Start();
